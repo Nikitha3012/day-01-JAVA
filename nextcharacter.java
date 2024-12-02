8a7a9f412e027246af259ccc8926dbafe64c6243
Nikitha3012 <nikkiroyal301205@gmail.com>
2024-12-02 15:15:24 +0530

    Add files via upload

---
+++ nextcharacter.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class nextcharacter
+{
+    public static void main(String[] args)
+    {
+        char c='a';
+        int b=c;
+        char v=(char)(b);
+        System.out.println(v);
+    }
+}
